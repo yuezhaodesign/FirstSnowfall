0d78e20fa8cf4d3e5659ffffef4f8d6759b9d0b8
Yue Zhao <131755643+yuezhaodesign@users.noreply.github.com>
2024-11-24 21:53:55 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const pics = document.querySelectorAll('#pic1, #pic2, #pic3, #pic4');
-    
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach((entry, index) => {
-            if (entry.isIntersecting) {
-                setTimeout(() => {
-                    entry.target.classList.add('visible');
-                }, index * 500); // Adjust the delay as needed
-                observer.unobserve(entry.target);
-            }
-        });
-    }, { threshold: 0.1 });
-
-    pics.forEach(pic => {
-        observer.observe(pic);
-    });
 
     const checkbox = document.getElementById('checkbox');
     const redirectUrl = 'https://ifttt.com/applets/wsSemthv-if-it-snows-send-a-confession-email-to-your-crush'; 
